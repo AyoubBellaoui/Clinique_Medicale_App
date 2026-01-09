fa3d4a6b58749ef6ef9db667a220b1f79ccd3bd0
AyoubBellaoui <Ayoubbellaoui37@gmail.com>
2026-01-09 19:42:52 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// import Express framework from node
+const express = require('express');
+
+// create an express server and refer it to app
+const app = express();
+
+// get Homepage route 
+app.get('/', (req, res) => {
+
+    res.send('Express App works good');
+
+});
+
+// Starts the server and Makes it listen for incoming requests
+const PORT = 3000;
+app.listen(PORT, () => {
+
+    // msg to confirm The server is alive
+    console.log(`Server running on http://localhost:${PORT}`);
+
+});
+
