599c702bfc4a1d3f6aa98defa9da48beed1cffd0
AyoubBellaoui <Ayoubbellaoui37@gmail.com>
2026-01-11 03:05:32 +0100

    changes on project

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 // import Express framework from node
 const express = require('express');
-
 // create an express server and refer it to app
 const app = express();
 
-// get Homepage route 
-app.get('/', (req, res) => {
+// Set view engine with EJS
+app.set('view engine', 'ejs');
 
-    res.send('Express App works good!');
-});
+// Middleware function (For any request, if the body is JSON, parse it)
+app.use(express.json());
+// later to understand
+app.use(express.urlencoded({ extended: true}));
+// Any file in public folder becomes accessible to the browser.
+app.use(express.static('public'));
+
+// when client req / sends it to auth routes
+const authroutes = require('./routes/auth.routes');
+app.use('/', authroutes);
 
 // Starts the server and Makes it listen for incoming requests
 const PORT = 3000;
-
 app.listen(PORT, () => {
 
     // confirm The server is alive
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
 
